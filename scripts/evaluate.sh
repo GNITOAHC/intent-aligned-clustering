bbafae283bb9dd2ae0a9588c68b7362aa10f0613
GINTOAHC <chaotingchen10@gmail.com>
2025-11-09 18:50:22 +0800

    feat: evaluation output file

--- scripts/evaluate.sh
+++ scripts/evaluate.sh
@@ -1,6 +1,7 @@
 PRED_DIR=$1
 GROUND_TRUTH_FILE=$2
+OUTPUT_FILE="${PRED_DIR}/evaluation.json"
 
-uv run python -m src.evaluate -p $PRED_DIR/out.csv -g $GROUND_TRUTH_FILE
+uv run python -m src.evaluate -p $PRED_DIR/out.csv -g $GROUND_TRUTH_FILE -o $OUTPUT_FILE
 
 # bash scripts/evaluate.sh out_gpt-oss-20b sample_gt.csv
